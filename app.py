aa80b491ae96eda265bfa33f0494b54f0aa33c79
Manali Jain <115955036+jmanali1996@users.noreply.github.com>
2024-02-29 20:38:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dash import Dash, html, dcc
 from dash_iconify import DashIconify
 
 intro_div = html.Div([
-    dmc.Image(src="https://i.im.ge/2024/02/29/gGL951.intro-pic.jpg", 
+    dmc.Image(src="https://im.ge/i/gGL951", 
                 width="720", height="405")
     ],
     style={"paddingTop": 40}                
@@ -26,7 +26,7 @@ pjcard1 = dmc.Card(
         dmc.CardSection(
             dmc.Anchor(
                 dmc.Image(
-                    src="https://i.im.ge/2024/02/29/gGL5rP.chatbot.jpg",
+                    src="https://im.ge/i/gGL5rP",
                     alt="Wild Bird Fund Chatbot",
                 ),
                 href="https://wbf-chatbot.onrender.com",
