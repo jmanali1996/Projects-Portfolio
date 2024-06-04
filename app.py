c54c961c1ac7faf4d00fec8f6919a436b87d7af0
Manali Jain <115955036+jmanali1996@users.noreply.github.com>
2024-06-04 12:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import dash_mantine_components as dmc
 import dash_bootstrap_components as dbc
-from dash import Dash, html, dcc
+from dash import Dash, html
 from dash_iconify import DashIconify
-from flask import Flask
 
 #INTRODUCTION
 intro_div = html.Div([
