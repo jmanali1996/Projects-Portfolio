b4784a80f745151f1e716beeea631c73aea0e9fd
Manali Jain <jmanali1996@gmail.com>
2025-05-20 07:46:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -743,4 +743,4 @@ def r_drawer(n_clicks):
     return True
 
 if __name__=='__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
